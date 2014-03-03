b4be21c71cbc437d6861a799e548a4b9e16bac36
Cong Hou <houcong1984@gmail.com>
2014-03-02 16:20:57 -0800

    A very simple prototype is finished.

--- roadtrip/roadtrip/settings.py
+++ roadtrip/roadtrip/settings.py
@@ -95,8 +95,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
+#STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
+STATIC_ROOT = ''
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, "static"),
+)
 
 MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
 MEDIA_URL = "/media/"
--- roadtrip/roadtrip/urls.py
+++ roadtrip/roadtrip/urls.py
@@ -3,18 +3,20 @@ from django.conf.urls import *
 from django.conf.urls import patterns
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^', include('cms.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+      url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+          {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+      url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
 
