fda2f0777cb73875982e1a85bad6c03d5baf199d
Cong Hou <houcong1984@gmail.com>
2014-04-02 00:19:17 -0700

    m

--- roadtrip/roadtrip/templatetags/roadtrip_tags.py
+++ roadtrip/roadtrip/templatetags/roadtrip_tags.py
@@ -16,12 +16,9 @@ def getTrails(context, poi_id):
 
 @register.assignment_tag(takes_context=True)
 def getPOIPhoto(context, poi_id):
-  print poi_id
   photos = POIPhoto.objects.all().filter(poi_id=poi_id)
-  print len(photos)
   if len(photos) == 0:
     return None
-  print photos[0].image.url
   return photos[0]
 
 @register.assignment_tag(takes_context=True)
@@ -29,10 +26,6 @@ def getPOIPhotoThumbnailURL(context, poi_id):
   photos = POIPhoto.objects.all().filter(poi_id=poi_id)
   if len(photos) == 0:
     return ""
-  print photos[0].image
-  print dir(photos[0])
-  print photos[0].get_absolute_url()
-  print photos[0].get_thumnnail_url()
   return photos[0].image
 
 @register.assignment_tag(takes_context=True)
